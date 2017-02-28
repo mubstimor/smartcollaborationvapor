3a43f22297957e0f44c04fa585d9f111345dcacc
Timothy Mubiru <mubstimor@gmail.com>
2017-02-28 22:08:12 +0000

    saving all changes

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,53 +1,38 @@
 import Vapor
 import VaporPostgreSQL
+import Auth
 
-let drop = Droplet(
-    preparations: [User.self],
-    providers: [VaporPostgreSQL.Provider.self]
-)
+let drop = Droplet()
 
-drop.get { request in
-    return try JSON(node: [
-        "message": "Hello, vapor!"
-        ])
-}
+drop.preparations.append(Country.self)
+drop.preparations.append(League.self)
+drop.preparations.append(Club.self)
+drop.preparations.append(Specialist.self)
+drop.preparations.append(Player.self)
+drop.preparations.append(Injury.self)
+drop.preparations.append(Treatment.self)
+drop.preparations.append(Subscription.self)
+drop.preparations.append(Feedback.self)
 
-drop.get("hello") { request in
-    return try JSON(node: [
-        "message": "Hello, again!"
-        ])
-}
+drop.addConfigurable(middleware: AuthMiddleware(user: Specialist.self), name: "auth")
 
-drop.get("players", Int.self) { request, players in
-    return try JSON(node: [
-        "message": "There are \(players) players here!"
-        ])
-}
+try drop.addProvider(VaporPostgreSQL.Provider.self)
 
-drop.post("players"){ request in
-    guard let name = request.data["name"]?.string else{
-        throw Abort.badRequest
-    }
-    return try JSON(node:[
-        "message":"Hello \(name)"
-        ])
-    
-}
 
-drop.get("dbversion") { request in
-    if let db = drop.database?.driver as? PostgreSQLDriver{
-        let version = try db.raw("SELECT version()")
-        return try JSON(node: version)
-    }else{
-        return "NO DB Connection"
-    }
-}
+let smart = SmartController()
+smart.addRoutes(drop: drop)
+
+let specialist = SpecialistController()
+specialist.addRoutes(drop: drop)
 
 drop.get("test"){ request in
 
-    var user = User(name:"Timothy Mubiru")
-    try user.save()
-    return try JSON(node: User.all().makeNode())
+//    var user = Specialist(email: "mubstimor@gmail.com", password: "sfdsrgeer", club: "Programmers United", country: "UK", league: "Devs")
+//    try user.save()
+//    return try JSON(node: Specialist.all().makeNode())
+    return try JSON(node: [
+        "message": "testing suspended temporarily!"
+        ])
 }
 
 drop.run()
