babf16c236512151e4c4266be52b89ed63ed6529
Timothy Mubiru <mubstimor@gmail.com>
2017-03-28 20:23:34 +0100

    format response test

--- Sources/App/Controllers/SmartController.swift
+++ Sources/App/Controllers/SmartController.swift
@@ -70,7 +70,8 @@ final class SmartController{
         let stripe = VaporStripe(apiKey: api_key, token: token)
         let result = try stripe.charge(amount: amountNo, currency: .usd, description: "Payment for subscription")
         //        let injuries = try Injury.query().filter("club_id", card_number).all()
-        return try JSON(node: ["message": "\(result)"])
+//        result.json
+        return try JSON(node: ["message": "\(result.json)"])
     }
     
     func appointments(request: Request) throws -> ResponseRepresentable{
