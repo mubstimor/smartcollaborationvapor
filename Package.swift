dc372982b18ba780a7fea151cbcd6d504f4a3400
Timothy Mubiru <mubstimor@gmail.com>
2017-03-29 13:08:57 +0100

    revert back

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
         .Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 0),
-        .Package(url:"https://github.com/anthonycastelli/vapor-stripe.git", majorVersion: 0, minor: 3)
+        .Package(url:"https://github.com/gomfucius/vapor-stripe.git", majorVersion: 0, minor: 2)
     ],
     exclude: [
         "Config",
